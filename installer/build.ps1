be5bc9f3f83d8fc09d116c8e43265a1f5b94613c
Taylor Monacelli <taylormonacelli@gmail.com>
2020-06-21 11:14:59 -0700

    working

--- installer/build.ps1
+++ installer/build.ps1
@@ -5,5 +5,8 @@ Copy-Item ../quser.psm1 Work -Force
 Copy-Item ../run_once.ps1 Work -Force
 Copy-Item ../run_once.vbs Work -Force
 
-candle.exe -nologo -ext WixUtilExtension -out test.wixobj -dVersion=1 test.wxs
-light.exe -out test.msi -sval test.wixobj -ext WixUIExtension
+candle.exe -dSourceFilesDir=Work .\product.wxs
+light.exe -out product.msi -sval product.wixobj -ext WixUIExtension
+# candle.exe -dSourceFilesDir=Work fragments.wxs
+# candle.exe -nologo -ext WixUtilExtension -out test.wixobj -dVersion=1 test.wxs
+# light.exe -out test.msi -sval test.wixobj -ext WixUIExtension
