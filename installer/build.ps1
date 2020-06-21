70f0b04bc9740df28d23fa516e49f8d2c0ee454e
Taylor Monacelli <taylormonacelli@gmail.com>
2020-06-21 09:56:48 -0700

    checkpoint

--- installer/build.ps1
+++ installer/build.ps1
@@ -6,4 +6,4 @@ Copy-Item ../run_once.ps1 Work -Force
 Copy-Item ../run_once.vbs Work -Force
 
 candle.exe -nologo -ext WixUtilExtension -out test.wixobj -dVersion=1 test.wxs
-# light.exe -out test.msi -sval test.wixobj -ext WixUIExtension
+light.exe -out test.msi -sval test.wixobj -ext WixUIExtension
