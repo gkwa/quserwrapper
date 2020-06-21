5c909aa44a4a81acad1e946bc255b7267334aba4
Taylor Monacelli <taylormonacelli@gmail.com>
2020-06-21 08:24:08 -0700

    windows_exporter example

--- installer/build.ps1
+++ installer/build.ps1
@@ -1,8 +1,9 @@
 mkdir -Force Work, Output | Out-Null
+
 Copy-Item ../main.ps1 Work -Force
 Copy-Item ../quser.psm1 Work -Force
 Copy-Item ../run_once.ps1 Work -Force
 Copy-Item ../run_once.vbs Work -Force
 
-
-candle.exe -nologo -ext WixUtilExtension -out Work\test.wixobj -dVersion=1 test.wxs
+candle.exe -nologo -ext WixUtilExtension -out test.wixobj -dVersion=1 test.wxs
+light.exe -out test.msi -sval test.wixobj -ext WixUIExtension
--- installer/do_only_once.ps1
+++ installer/do_only_once.ps1
@@ -6,7 +6,8 @@ Copy-Item ../quser.psm1 Work -Force
 $guid1=(new-guid).guid
 $guid2=(new-guid).guid
 
-heat.exe dir Work -nologo -ag -cg myCG -sreg -template product -out test.wxs
+# heat.exe dir Work -nologo -ag -cg myCG -sreg -template product -out test.wxs
+heat.exe dir Work -nologo -out test.wxs
 (Get-Content test.wxs) `
  -replace 'Title="PUT-FEATURE-TITLE-HERE"', 'Title="Quserwrapper"' `
  -replace 'PUT-PRODUCT-NAME-HERE', 'Quserwrapper' `
