03d058015dfd156915558a435617fadff1fcb27d
Taylor Monacelli <taylormonacelli@gmail.com>
2020-06-21 14:29:46 -0700

    t

--- installer/build.ps1
+++ installer/build.ps1
@@ -5,8 +5,5 @@ Copy-Item ../run_once.ps1 Work -Force
 Copy-Item ../run_once.vbs Work -Force
 Copy-Item ../quser.psm1 Work -Force
 
-$guid1=(new-guid).guid
-$guid2=(new-guid).guid
-
-candle.exe -nologo quserwrapper.wxs 
-light.exe -nologo -ext WixUIExtension quserwrapper.wixobj -out Output/quserwrapper.msi
+candle.exe -nologo product.wxs 
+light.exe -nologo -ext WixUIExtension product.wixobj -out Output/product.msi
--- installer/do_once_only.ps1
+++ installer/do_once_only.ps1
@@ -1,9 +1,9 @@
 $guid1=(new-guid).guid
 $guid2=(new-guid).guid
 
-(Get-Content quserwrapper.wxs) `
+(Get-Content product.wxs) `
  -replace 'Title="PUT-FEATURE-TITLE-HERE"', 'Title="MyFeatureTitleName"' `
  -replace 'PUT-PRODUCT-NAME-HERE', 'MyProductName' `
  -replace 'PUT-COMPANY-NAME-HERE', 'MyCompanyName' `
  -replace 'UpgradeCode="PUT-GUID-HERE"', "UpgradeCode=`"$guid1`"" `
- -replace 'Id="PUT-GUID-HERE"', "Id=`"$guid2`"" | Set-Content -Path quserwrapper.wxs
+ -replace 'Id="PUT-GUID-HERE"', "Id=`"$guid2`"" | Set-Content -Path product.wxs
