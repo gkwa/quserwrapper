33c9fb81c235198518a41b96c544700a445650d7
Taylor Monacelli <taylormonacelli@gmail.com>
2020-06-21 08:32:20 -0700

    use windows_exporter as template and replace with my files

--- installer/do_only_once.ps1
+++ installer/do_only_once.ps1
@@ -7,10 +7,10 @@ $guid1=(new-guid).guid
 $guid2=(new-guid).guid
 
 # heat.exe dir Work -nologo -ag -cg myCG -sreg -template product -out test.wxs
-heat.exe dir Work -nologo -out test.wxs
+# heat.exe dir Work -nologo -out test.wxs
 (Get-Content test.wxs) `
  -replace 'Title="PUT-FEATURE-TITLE-HERE"', 'Title="Quserwrapper"' `
  -replace 'PUT-PRODUCT-NAME-HERE', 'Quserwrapper' `
  -replace 'PUT-COMPANY-NAME-HERE', 'Streambox' `
- -replace 'UpgradeCode="PUT-GUID-HERE"', "UpgradeCode=`"{$guid1}`"" `
- -replace 'Id="PUT-GUID-HERE"', "Id=`"{$guid2}`"" | Set-Content -Path test.wxs
+ -replace 'UpgradeCode="PUT-GUID-HERE"', "UpgradeCode=`"$guid1`"" `
+ -replace 'Id="PUT-GUID-HERE"', "Id=`"$guid2`"" | Set-Content -Path test.wxs
