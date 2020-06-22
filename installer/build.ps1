979977949900f40d57f20430e84830b079c3815d
Taylor Monacelli <taylormonacelli@gmail.com>
2020-06-21 17:05:21 -0700

    [skip ci] yada-yada will rebase when working

--- installer/build.ps1
+++ installer/build.ps1
@@ -1,9 +1,26 @@
 mkdir -Force Work, Output | Out-Null
 
+Copy-Item -Force product.wxs.tmpl product.wxs
+
 Copy-Item ../main.ps1 Work -Force
 Copy-Item ../run_once.ps1 Work -Force
 Copy-Item ../run_once.vbs Work -Force
 Copy-Item ../quser.psm1 Work -Force
 
-candle.exe -nologo product.wxs 
-light.exe -nologo -ext WixUIExtension product.wixobj -out Output/product.msi
+$guid1=(new-guid).guid
+$guid2=(new-guid).guid
+
+(Get-Content product.wxs) `
+ -replace 'Title="PUT-FEATURE-TITLE-HERE"', 'Title="MyFeatureTitleName"' `
+ -replace 'PUT-PRODUCT-NAME-HERE', 'MyProductName' `
+ -replace 'PUT-COMPANY-NAME-HERE', 'MyCompanyName' `
+ -replace 'UpgradeCode="PUT-GUID-HERE"', "UpgradeCode=`"$guid1`"" `
+ -replace 'Id="PUT-GUID-HERE"', "Id=`"$guid2`"" | Set-Content -Path product.wxs
+
+heat.exe dir Work -var var.SourceFilesDir -out Components.wxs `
+ -cg MyComponentGroup -dr APPLICATIONROOTDIRECTORY -g1 -gg -srd -nologo
+
+candle.exe -nologo -dSourceFilesDir=Work Components.wxs
+candle.exe -nologo product.wxs
+
+light.exe -nologo -ext WixUIExtension Components.wixobj product.wixobj -out Output/product.msi
--- installer/do_once_only.ps1
+++
@@ -1,9 +0,0 @@
-$guid1=(new-guid).guid
-$guid2=(new-guid).guid
-
-(Get-Content product.wxs) `
- -replace 'Title="PUT-FEATURE-TITLE-HERE"', 'Title="MyFeatureTitleName"' `
- -replace 'PUT-PRODUCT-NAME-HERE', 'MyProductName' `
- -replace 'PUT-COMPANY-NAME-HERE', 'MyCompanyName' `
- -replace 'UpgradeCode="PUT-GUID-HERE"', "UpgradeCode=`"$guid1`"" `
- -replace 'Id="PUT-GUID-HERE"', "Id=`"$guid2`"" | Set-Content -Path product.wxs
