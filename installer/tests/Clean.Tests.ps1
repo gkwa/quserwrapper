b309f46cd3120eb3a3f35c5f4bf8eedcfe4f9147
Taylor Monacelli <taylormonacelli@gmail.com>
2020-07-09 10:25:13 -0700

    rename run_once to main

--- installer/tests/Clean.Tests.ps1
+++ installer/tests/Clean.Tests.ps1
@@ -6,9 +6,9 @@ Describe "Install" {
     It "verify product was installed into correct directory" {
         Test-Path "C:\Program Files (x86)\MyCompanyName\MyProductName" | Should -Be $true
         Test-Path "C:\Program Files (x86)\MyCompanyName\MyProductName\main.ps1" | Should -Be $true
-        Test-Path "C:\Program Files (x86)\MyCompanyName\MyProductName\run_once.ps1" | Should -Be $true
+        Test-Path "C:\Program Files (x86)\MyCompanyName\MyProductName\main.ps1" | Should -Be $true
         Test-Path "C:\Program Files (x86)\MyCompanyName\MyProductName\quser.psm1" | Should -Be $true
-        Test-Path "C:\Program Files (x86)\MyCompanyName\MyProductName\run_once.vbs" | Should -Be $true
+        Test-Path "C:\Program Files (x86)\MyCompanyName\MyProductName\main.vbs" | Should -Be $true
     }
 }
 
@@ -16,8 +16,8 @@ Describe "Uninstall" {
     It "verify files and directory no longer exist" {
         Test-Path "C:\Program Files (x86)\MyCompanyName\MyProductName" | Should -Be $false
         Test-Path "C:\Program Files (x86)\MyCompanyName\MyProductName\main.ps1" | Should -Be $false
-        Test-Path "C:\Program Files (x86)\MyCompanyName\MyProductName\run_once.ps1" | Should -Be $false
+        Test-Path "C:\Program Files (x86)\MyCompanyName\MyProductName\main.ps1" | Should -Be $false
         Test-Path "C:\Program Files (x86)\MyCompanyName\MyProductName\quser.psm1" | Should -Be $false
-        Test-Path "C:\Program Files (x86)\MyCompanyName\MyProductName\run_once.vbs" | Should -Be $false
+        Test-Path "C:\Program Files (x86)\MyCompanyName\MyProductName\main.vbs" | Should -Be $false
     }
 }
