1c6daad6326099b42628cb972438f59c7c3a1c09
Taylor Monacelli <taylormonacelli@gmail.com>
2020-07-12 14:27:14 -0700

    [skip ci] yada-yada will rebase when working

--- installer/build.ps1
+++ installer/build.ps1
@@ -12,8 +12,8 @@ $guid2=(new-guid).guid
 
 (Get-Content product.wxs) `
  -replace 'Title="PUT-FEATURE-TITLE-HERE"', 'Title="MyFeatureTitleName"' `
- -replace 'PUT-PRODUCT-NAME-HERE', 'MyProductName' `
- -replace 'PUT-COMPANY-NAME-HERE', 'MyCompanyName' `
+ -replace 'PUT-PRODUCT-NAME-HERE', 'Quserwrapper' `
+ -replace 'PUT-COMPANY-NAME-HERE', 'Taylorm' `
  -replace 'UpgradeCode="PUT-GUID-HERE"', "UpgradeCode=`"$guid1`"" `
  -replace 'Id="PUT-GUID-HERE"', "Id=`"$guid2`"" | Set-Content -Path product.wxs
 
